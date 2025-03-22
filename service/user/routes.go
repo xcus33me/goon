8d0182fd72573a8d348e37823de7af5c23e17ad2
xcus33me <HarvieOrigin@yandex.ru>
2025-03-23 00:33:11 +0300

    new

--- service/user/routes.go
+++
@@ -1,8 +0,0 @@
-package user
-
-import "github.com/gin-gonic/gin"
-
-func (h *Handler) RegisterRoutes(router *gin.RouterGroup) {
-	router.POST("/login", h.handleLogin)
-	router.POST("/register", h.handleRegister)
-}
