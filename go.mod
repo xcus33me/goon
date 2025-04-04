32b7fb21753a280e2b4973c58d73e2a2c7db52ae
xcus33me <HarvieOrigin@yandex.ru>
2025-04-04 22:57:07 +0300

    123

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module goon
 
 go 1.23.4
+
+require (
+	github.com/google/uuid v1.6.0
+	github.com/jackc/pgx v3.6.2+incompatible
+)
+
+require (
+	github.com/gofrs/uuid v4.4.0+incompatible // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+)
---
+++ services/api_gateway/internal/service/chat.go
@@ -0,0 +1,19 @@
+package service
+
+import (
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type UserClient struct {
+	conn   *grpc.ClientConn
+	client pb.UserServiceClient
+}
+
+func NewUserClient(address string) (*UserClient, error) {
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+	client := pb
+}
